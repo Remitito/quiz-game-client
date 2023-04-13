0737626113928b910d654ca57974b3ee18fa84f4
Remitito <JRossOT@protonmail.com>
2023-04-13 09:40:47 +0800

    Improve navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import {Col, Row, Avatar, Space, Button} from 'antd'
-import {FindQuizButton} from '../assets/styledComponents/Buttons' 
 import '../assets/stylesheets/navbar.css'
 import {UserOutlined, SearchOutlined} from '@ant-design/icons'
 
 export const Navbar = () => {
     return (
         <Row className='navbarCont'>
-            <Col span={8}>
-                <FindQuizButton className='findQuiz'>
-                <SearchOutlined className='searchIcon' />
-                    Find a quiz
-                </FindQuizButton>
+            <Col span={8} className='findCont'>
+            <Row>
+                <Space>
+                    <Avatar size="large" className="navbarIcon" icon={<SearchOutlined/>}/>
+                    <label className='findCont'>Find a quiz</label>
+                </Space>
+            </Row>
             </Col>
             <Col className='titleCont' span={8}>
                 <h1 className='title'>Quiz Game</h1>
@@ -18,7 +19,7 @@ export const Navbar = () => {
             <Col className='accountCont' span={8}>
                 <Row>
                     <Space>
-                        <Avatar icon={<UserOutlined/>}/>
+                        <Avatar size="large" className="navbarIcon" icon={<UserOutlined/>}/>
                         <label className='accountCont'>Guest User</label>
                     </Space>
                 </Row>
